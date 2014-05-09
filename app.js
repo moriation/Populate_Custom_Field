e2570d4219cb29b3509254dfb13f3c73727d780b
kobeddon <awsmori@yahoo.com>
2014-05-09 23:49:00 +0800

    remove the line of alert yo
    remove the line of alert yo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
     	var ticket = this.ticket();
 		ticket.assignee().user();
     	alert(ticket.assignee().user().email());
-    	//alert("yo");
+
     	//ticket.customField("custom_field_23790083").;
     	ticket.customField("custom_field_23790083", value = ticket.assignee().user().email()); 
 
