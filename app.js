7791ac98b5665ef5dea02a4902b5a9a8a659171c
moriation <amori@zendesk.com>
2014-05-09 23:31:45 +0800

    first commit
    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+(function() {
+
+  return {
+    events: {
+      'app.activated':'doSomething'
+    },
+
+    doSomething: function() {
+    	var ticket = this.ticket();
+		ticket.assignee().user();
+    	alert(ticket.assignee().user().email());
+    	alert("yo");
+    	//ticket.customField("custom_field_23790083").;
+    	ticket.customField("custom_field_23790083", value = ticket.assignee().user().email()); 
+
+    }
+  };
+
+}());
